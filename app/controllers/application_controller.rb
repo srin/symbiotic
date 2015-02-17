ca804edf9fed4fb5e574fe984e255bb4c2bf0e22
srin <smadipalli@hotmail.com>
2015-02-17 17:40:52 +0000

    mailer debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,16 @@ protected
    devise_parameter_sanitizer.for(:account_update) << :image
  end
 
+  
+end
+
   def list_posts
   @q = Post.ransack(params[:q])
   end
-end
+
+  private
+
+  def make_action_mailer_use_request_host_and_protocol
+    ActionMailer::Base.default_url_options[:protocol] = request.protocol
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
